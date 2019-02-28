b1fbf1368b722e20811e8e92fd45cf38a81dc89a
Austin <joha0033@gmail.com>
2019-02-28 12:34:02 -0700

    linted

---
+++ src/App.jsx
@@ -0,0 +1,343 @@
+import React, {
+  useState, useEffect, useReducer, useRef,
+} from 'react';
+import './App.css';
+import PropTypes from 'prop-types';
+import {
+  FaPlay,
+  FaPause,
+  FaChevronCircleRight,
+  FaChevronCircleLeft,
+  FaCircle,
+} from 'react-icons/fa';
+
+// navigator.mediaDevices.getUserMedia({ audio: true })
+//   .then((stream) => {
+//     const mediaRecorder = new MediaRecorder(stream);
+//     mediaRecorder.start();
+//   });
+
+export const SLIDE_DURATION = 30000;
+
+export function Slide({
+  isCurrent,
+  takeFocus,
+  slide,
+}) {
+  const ref = useRef();
+  useEffect(() => {
+    if (isCurrent && takeFocus) {
+      ref.current.focus();
+    }
+  }, [isCurrent, takeFocus]);
+
+  return (
+    <li
+      ref={ref}
+      aria-hidden={!isCurrent}
+      tabIndex="-1"
+      className="Slide"
+      style={{ backgroundImage: `url(${slide.url})` }}
+    />
+  );
+}
+
+Slide.propTypes = {
+  isCurrent: PropTypes.bool.isRequired,
+  takeFocus: PropTypes.bool.isRequired,
+  slide: PropTypes.shape({
+    url: PropTypes.string,
+  }).isRequired,
+};
+
+export function Slides(props) {
+  return (
+    <ul className="Slides" {...props} />
+  );
+}
+
+export function Carousel(props) {
+  return (
+    <section className="Carousel" {...props} />
+  );
+}
+
+export function Controls(props) {
+  return <div className="Controls" {...props} />;
+}
+
+export function SlideNav(props) {
+  return <div className="SlideNav" {...props} />;
+}
+
+export function SlideNavItem(props) {
+  const { isCurrent } = props;
+  return isCurrent
+    ? <button type="button" className="SlideNavItemOn" {...props} />
+    : <button type="button" className="SlideNavItemOff" {...props} />;
+}
+
+export function IconButton(props) {
+  const { name } = props;
+  return name === 'Play'
+    ? <button type="button" className="IconButtonLarge" {...props} />
+    : <button type="button" className="IconButton" {...props} />;
+}
+
+export function SpacerGif({ width }) {
+  return (
+    <div
+      style={{ display: 'inline-block', width }}
+    />
+  );
+}
+
+SpacerGif.propTypes = {
+  width: PropTypes.string.isRequired,
+};
+
+
+export const useProgress = (animate, time) => {
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    let rafId = null;
+    if (animate) {
+      let start = null;
+      const step = (timestamp) => {
+        if (!start) start = timestamp;
+        setProgress(timestamp - start);
+        if (progress < time) {
+          rafId = requestAnimationFrame(step);
+        }
+      };
+      rafId = requestAnimationFrame(step);
+    }
+
+    return () => cancelAnimationFrame(rafId);
+  }, [time, animate]);
+
+  return animate
+    ? Math.min(progress / time, time)
+    : 0;
+};
+
+export function ProgressBar({ animate, time, lastSlide }) {
+  const progressUsed = useProgress(animate, time, lastSlide);
+
+  return (
+    <div className="ProgressBar">
+      <div
+        style={{ width: `${progressUsed * 100}%` }}
+      />
+    </div>);
+}
+
+ProgressBar.propTypes = {
+  animate: PropTypes.bool.isRequired,
+  time: PropTypes.number.isRequired,
+  lastSlide: PropTypes.bool.isRequired,
+};
+
+export default function App() {
+  // eslint-disable-next-line no-shadow
+  const [state, dispatch] = useReducer((state, action) => {
+    switch (action.type) {
+      case 'PROGRESS':
+      case 'NEXT': return {
+        ...state,
+        isPlaying: action.type === 'PROGRESS',
+        takeFocus: false,
+        currentIndex: (state.currentIndex + 1) % state.slides.length,
+      };
+      case 'PREV': return {
+        ...state,
+        isPlaying: false,
+        takeFocus: false,
+        currentIndex: (state.currentIndex - 1 + state.slides.length) % state.slides.length,
+      };
+      case 'PLAY': return {
+        ...state,
+        takeFocus: false,
+        isPlaying: true,
+      };
+      case 'PAUSE': return {
+        ...state,
+        takeFocus: false,
+        isPlaying: false,
+      };
+      case 'GOTO': return {
+        ...state,
+        takeFocus: true,
+        currentIndex: action.index,
+      };
+      case 'FETCH_SUCCESS': return {
+        ...state,
+        loading: false,
+        slides: action.slides,
+      };
+      case 'RESTART': return {
+        ...state,
+        start: !state.start,
+        loading: true,
+        slides: console.log('a new start'),
+      };
+      case 'FINISHED': return {
+        ...state,
+        isPlaying: false,
+        loading: true,
+        slides: console.log('insert finishing slide??'),
+      };
+      default: return state;
+    }
+  }, {
+    currentIndex: 0,
+    slides: ['empty'],
+    loading: true,
+    isPlaying: false,
+    takeFocus: false,
+    start: false,
+  });
+
+  useEffect(() => {
+    if (!state.loading && state.currentIndex === state.slides.length - 1) {
+      setTimeout(() => {
+        dispatch({ type: 'FINISHED' });
+      }, SLIDE_DURATION - 50);
+    }
+  }, [state.currentIndex]);
+
+  const fetchSlides = async () => {
+    const response = await fetch('https://photo-api-2019.herokuapp.com/images/array_of/3/')
+      .then(res => res.json())
+      .then((data) => {
+        dispatch({ type: 'FETCH_SUCCESS', slides: data.images });
+        return data;
+      });
+    return response;
+  };
+
+  useEffect(() => { fetchSlides(); }, [state.restart]);
+
+  useEffect(() => {
+    let timeout;
+    if (state.isPlaying) {
+      timeout = setTimeout(() => {
+        dispatch({ type: 'PROGRESS' });
+      }, SLIDE_DURATION);
+    }
+    return () => { clearTimeout(timeout); };
+  }, [state.currentIndex, state.isPlaying]);
+
+  return (
+    state.loading
+      ? (
+        <div className="PlayAgain">
+          <h1 className="Loading">Pat your self on the back and refresh for another round!</h1>
+        </div>
+      ) // some kind of dynamic component... ? start, loading, finished...
+      : (
+        <div className="App">
+          <header className="App-header">
+            <Carousel>
+
+              <Slides>
+                {
+                    state.slides.map((slide, index) => (
+                      <Slide
+                        isCurrent={index === state.currentIndex}
+                        key={slide.id}
+                        slide={slide}
+                        takeFocus={state.takeFocus}
+                      />
+                    ))
+                  }
+              </Slides>
+
+              <SlideNav>
+                {
+                    state.slides.map((slide, index) => (index === state.currentIndex
+                      ? (
+                        <SlideNavItem
+                          key={slide.id}
+                          aria-label={`Slide ${index + 1}`}
+                          onClick={() => {
+                            dispatch({ type: 'GOTO', index });
+                          }}
+                        >
+                          <FaCircle style={{ opacity: 1 }} />
+                        </SlideNavItem>
+                      )
+                      : (
+                        <SlideNavItem
+                          key={slide.id}
+                          aria-label={`Slide ${index + 1}`}
+                          onClick={() => {
+                            dispatch({ type: 'GOTO', index });
+                          }}
+                        >
+                          <FaCircle style={{ opacity: 0.6 }} />
+                        </SlideNavItem>
+                      )))
+                  }
+              </SlideNav>
+
+              <Controls>
+                {
+                    state.isPlaying
+                      ? (
+                        <IconButton
+                          name="Pause"
+                          aria-label="Pause"
+                          onClick={() => { dispatch({ type: 'PAUSE' }); }}
+                        >
+                          <FaPause />
+                        </IconButton>
+                      )
+                      : (
+                        <IconButton
+                          name="Play"
+                          aria-label="Play"
+                          onClick={() => {
+                            dispatch({ type: 'PLAY' });
+                          }}
+                        >
+                          <FaPlay />
+                        </IconButton>
+                      )
+                  }
+                <SpacerGif width="10px" />
+                <IconButton
+                  aria-label="Previous Slide"
+                  onClick={() => {
+                    dispatch({ type: 'PREV' });
+                  }}
+                >
+                  <FaChevronCircleLeft />
+                </IconButton>
+                <SpacerGif width="10px" />
+                <IconButton
+                  aria-label="Next Slide"
+                  onClick={() => {
+                    dispatch({ type: 'NEXT' });
+                  }}
+                >
+                  <FaChevronCircleRight />
+                </IconButton>
+              </Controls>
+
+              <ProgressBar
+                key={state.currentIndex + state.isPlaying}
+                time={SLIDE_DURATION}
+                animate={state.isPlaying}
+                lastSlide={state.currentIndex === state.slides.length - 1}
+              />
+
+            </Carousel>
+          </header>
+        </div>
+      ));
+}
+
+
+// export default App;
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from '../App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
+  // eslint-disable-next-line react/jsx-filename-extension
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
